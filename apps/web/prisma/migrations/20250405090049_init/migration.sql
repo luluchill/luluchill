7fbca569ba3999e5e62daf61d8040c86f47fa9aa
kth-tw <88102592+kth-tw@users.noreply.github.com>
2025-04-05 17:00:56 +0800

    Add Prisma integration and user model; Create User to DB when self-protocol verified

---
+++ apps/web/prisma/migrations/20250405090049_init/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "ethAddress" TEXT NOT NULL,
+    "passportNumber" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "olderThan" TEXT NOT NULL,
+    "nationality" TEXT NOT NULL,
+    "passportNoOfac" BOOLEAN NOT NULL,
+    "isEasIssued" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_ethAddress_key" ON "User"("ethAddress");
