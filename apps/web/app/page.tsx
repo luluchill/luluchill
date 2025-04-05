3580df967788a0b2e781b854a50f2c0233a8f264
Spark Huang <50857240+sparkdoaz@users.noreply.github.com>
2025-04-05 20:14:03 +0800

    add path \ wallet check

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   if (currentAddress === institutionAddress) {
     redirect("/institution/review"); // 如果是機構，直接重定向到 dashboard
       } else {
-        redirect("/"); // 如果不是機構，重定向到首頁
+        // redirect("/"); // 如果不是機構，重定向到首頁
   }
     } else {
       redirect("/"); // 如果沒有連接錢包，重定向到首頁
