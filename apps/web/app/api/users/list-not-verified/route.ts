c1b29b1e0a6ec4f57c108cf70147e9d4ff85ed73
kth-tw <88102592+kth-tw@users.noreply.github.com>
2025-04-05 13:39:33 +0800

    Add API endpoint to fetch users not verified by EAS

---
+++ apps/web/app/api/users/list-not-verified/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "../../../../generated/prisma";
+
+const prisma = new PrismaClient();
+export async function GET() {
+  try {
+    const users = await prisma.user.findMany({
+      where: {
+        isEasIssued: false,
+      },
+    });
+
+    return NextResponse.json(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch users" },
+      { status: 500 }
+    );
+  }
+}
